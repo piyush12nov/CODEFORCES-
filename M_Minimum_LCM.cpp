a7124a8bed0fcd5075d9fbc3e849ba79c24abacc
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2022-11-28 21:39:11 +0530

    Add files via upload
    2022-2023 ICPC, NERC, Southern and Volga Russian Regional Contest (Online Mirror, ICPC Rules, Preferably Teams)

---
+++ M_Minimum_LCM.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define db double
+ll i;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		ll n;
+		cin >> n;
+
+		ll key = 1;
+		for (i = 2; i * i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				key=max({key,i,n/i});
+			}
+		}
+
+		cout << key << " " << n-key << endl;
+	}
+
+	return 0;
+}
